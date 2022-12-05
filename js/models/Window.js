92eb69500b58112d16f8c52330b671f7b964d93c
Gianfranco <giacocipo@gmail.com>
2022-12-05 19:55:53 -0300

    bronce difuce fix

--- js/models/Window.js
+++ js/models/Window.js
@@ -8,7 +8,7 @@ class Window extends Cube {
 
         let materialGlass = new Material("./assets/textures/glass/TexturesCom_HighRiseGlass0069_1_seamless_S.jpg", 0.25, 1., 15, 1);
 
-        let material = new Material("/assets/textures/Bronze01_MR_1K/Bronze01_1K_BaseColor.png", 0.5, 1., 30, 0.2);
+        let material = new Material("./assets/textures/Bronze01_MR_1K/Bronze01_1K_BaseColor.png", 0.5, 1., 30, 0.2);
 
         let materials = {
             'topT': material,
